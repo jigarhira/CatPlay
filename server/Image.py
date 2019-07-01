60beed5140f08105aff53767fdacbcbc4249eb3a
JIGAR-XPS\Jigar <hiraj@uci.edu>
2019-06-30 21:13:58 -0700

    Added ffmpeg piping

--- server/Image.py
+++ server/Image.py
@@ -6,8 +6,38 @@
 /*************'''
 
 
+import subprocess
+
+# get video stream from camera
+def get_video():
+    ffmpeg_command = ['ffmpeg',
+                      '-f', 'dshow',
+                      '-i', 'video=Integrated Webcam',
+                      '-f', 'h264',
+                      'pipe:']
+    ffmpeg = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE)
+    return ffmpeg.stdout
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Unit test
 if __name__ == '__main__':
-    pass
+    get_video()
 
 # EOF
